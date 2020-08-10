21aca53fb980ea9899070b1098b5b72c29a65433
zihadmahiuddin <zihadmahiuddin@gmail.com>
2020-08-10 09:52:23 +0000

    Deploying to gh-pages from  @ 0ed551fab988c9508ae51fe0e08def83a19da1bc 🚀

---
+++ module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"Spigotlin","url":"index.html"}]
---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"cf.zihad.spigotlin","url":"cf.zihad.spigotlin/package-summary.html"}, {"l":"All packages","url":"index.html"}]
