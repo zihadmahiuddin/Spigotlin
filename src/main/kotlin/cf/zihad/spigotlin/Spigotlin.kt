869ef59d85734b7c2e1afe4f22fbcdae0de35b1c
Mahi-Uddin Zihad <zihadmahiuddin@gmail.com>
2020-08-10 10:24:12 +0600

    Javadoc _kinda_

--- src/main/kotlin/cf/zihad/spigotlin/Spigotlin.kt
+++ src/main/kotlin/cf/zihad/spigotlin/Spigotlin.kt
@@ -2,18 +2,35 @@ package cf.zihad.spigotlin
 
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * An example Spigot plugin written in Kotlin that includes docs with Dokka.
+ * @author Zihad
+ */
 @Suppress("UNUSED")
 class Spigotlin : JavaPlugin() {
+    /**
+     * The function that's called when the plugin is enabled by the server.
+     */
     override fun onEnable() {
         logger.info("Enabled $NAME v$VERSION!")
     }
 
+    /**
+     * The function that's called when the plugin is disabled by the server.
+     */
     override fun onDisable() {
         logger.info("Disabled $NAME=rf  v$VERSION!")
     }
 
     companion object {
+        /**
+         * The name of the plugin.
+         */
         const val NAME = "Spigotlin"
+
+        /**
+         * The version of the plugin.
+         */
         const val VERSION = "1.0"
     }
 }
